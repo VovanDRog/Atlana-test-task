6abc4a3d4eb37781fb91fbd813d6eba9cd039f87
VovanDRog <bobgrean97@gmail.com>
2021-02-07 08:35:07 +0200

    save token in env

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 const config: AxiosRequestConfig = {
   baseURL: "https://api.github.com/",
   headers: {
-    Authorization: "token faffb12b19f421c6d87fbf6c69ea5ec3dc4c2efd",
+    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
   },
 };
 
