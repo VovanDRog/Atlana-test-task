0fac41d69c5228b53af702965586fa4034e44d07
VovanDRog <bobgrean97@gmail.com>
2021-02-07 10:40:08 +0200

    update axios config

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,10 +1,14 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
+const headers: any = {};
+const token = process.env.REACT_APP_GITHUB_TOKEN;
+if (token) {
+  headers["Authorization"] = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
+}
+
 const config: AxiosRequestConfig = {
   baseURL: "https://api.github.com/",
-  headers: {
-    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-  },
+  headers,
 };
 
 const client: AxiosInstance = axios.create(config);
